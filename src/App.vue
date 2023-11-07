ae3969c83bc0ea3feeed62e3400cf98ac755b810
zh_off <>
2023-11-07 14:34:02 +0100

    fix: upstream change in api

--- src/App.vue
+++ src/App.vue
@@ -24,8 +24,10 @@ function generateUrl(path, line_num = 0, new_window = true) {
 
   const windowId = new_window ? "?windowId=_blank" : "";
 
+  // TODO remove in the next version of vscode 1.84.0
+  const _path = encodeURIComponent(path).replace((/^\/*/g), "");
 
-  return `${protocol}://file/` + encodeURIComponent(path) + line + windowId;
+  return `${protocol}://file/` + _path + line + windowId;
 }
 
 async function openConfig(name) {
